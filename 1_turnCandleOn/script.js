d442afe35f6d29aaece7a9ed1b14fb8872875fca
Jonathan <many.d.r.o.2001@hotmail.com>
2021-09-17 16:49:51 -0500

    Turning a candle on has been done

---
+++ 1_turnCandleOn/script.js
@@ -0,0 +1,21 @@
+let candle;
+const yes = document.getElementById('yes');
+const no = document.getElementById('no');
+
+yes.addEventListener('click', function(){Yes()});
+no.addEventListener('click', function(){No()});
+
+function Yes()
+{
+    alert("Great! it's on");
+}
+
+function No()
+{
+    alert("You should search for matchsticks");
+    do
+    {
+        correct = prompt("Have you found some?(y/n)");
+    }while((correct != 'n' && correct != 'y') || correct == 'n')
+    Yes();
+}
